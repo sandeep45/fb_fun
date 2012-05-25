bc37068e3f2ef3a9f29616fc749003a7653c3c6e
Sandeep Arneja <sandeep45@gmail.com>
2012-05-25 19:03:05 -0400

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DollarMaker::Application.config.secret_token = '36b15b9dbc1935675860e09aec148d7e684cbb7e64537e2bd37e1e083c39dda9503b6514555c17eda29e5a6c438b4f76c787557032cb4017d7b930c412cb8f2d'
